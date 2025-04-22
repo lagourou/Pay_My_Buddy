e345d1ddc207e9c60de7cc958f053e91e3adde62
lagourou <agourou84@gmail.com>
2025-04-22 11:32:23 +0200

    Ajout des nouveaux fichiers et mise à jour du projet

---
+++ src/main/java/com/pay_my_buddy/OC_P6/OcP6Application.java
@@ -0,0 +1,13 @@
+package com.pay_my_buddy.OC_P6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcP6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcP6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay_my_buddy/OC_P6/OcP6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay_my_buddy.OC_P6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcP6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
