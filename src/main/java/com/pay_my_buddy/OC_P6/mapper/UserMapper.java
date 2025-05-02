b7cca8ad28c7b9574164ad8be125964d15eedf7b
lagourou <agourou84@gmail.com>
2025-05-02 11:38:53 +0200

    UserMapper convertit les objets User en DTO afin d'éviter d'exposer directement les entitésde la bdd empêche aussi les modifications directes des objets comme User, grâce à ça on transfert les données importantes comme username, email, password tout en ignorants les autres comme friends, accountBalance et facilite le transfert entre le frontend et le backend

---
+++ src/main/java/com/pay_my_buddy/OC_P6/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.pay_my_buddy.OC_P6.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.pay_my_buddy.OC_P6.dto.RegisterRequestDTO;
+import com.pay_my_buddy.OC_P6.model.User;
+
+@Mapper
+public interface UserMapper {
+
+    public RegisterRequestDTO toRegisterRequestDTO(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "friends", ignore = true)
+    @Mapping(target = "receivedTransactions", ignore = true)
+    @Mapping(target = "sentTransactions", ignore = true)
+    @Mapping(target = "accountBalance", ignore = true)
+
+    public User toUser(RegisterRequestDTO registerRequestDTO);
+
+}
